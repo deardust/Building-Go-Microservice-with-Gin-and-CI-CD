a327f65ffd4357e27312cdf6272a06f8b363a22f
王亚非 <yh786128909@icloud.com>
2022-10-21 11:02:26 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Set the router as the default one provided by Gin
+	router := gin.Default()
+	// Process the templates at the start so that they don't have to be loaded
+	// from the disk again. This makes serving HTML pages very fast.
+	router.LoadHTMLGlob("templates/*")
+	// Handle Index
+	router.GET("/", showIndexPage)
+	//Handel GET requests at
+	// Handle GET requests at /article/view/some_article_id
+	router.GET("/article/view/:article_id", getArticle)
+	router.Run()
+}
